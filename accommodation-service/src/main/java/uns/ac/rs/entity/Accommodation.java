b2d61f077f969b4ee0e3ceab57a48814d34c0539
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-02 15:16:16 +0200

    feat: added basic crud operations for accommodation

--- accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
+++ accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
@@ -1,4 +1,34 @@
 package uns.ac.rs.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookit_accommodations")
 public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private String filters;
+    private int minGuests;
+    private int maxGuests;
+    private String priceType;
+
+    @OneToMany(mappedBy = "accommodation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PriceAdjustment> priceAdjustments;
+
+    @ElementCollection
+    @CollectionTable(name = "images", joinColumns = @JoinColumn(name = "accommodation_id"))
+    private List<String> images;
 }
--- accommodation-service/src/main/java/uns/ac/rs/service/AccommodationService.java
+++ accommodation-service/src/main/java/uns/ac/rs/service/AccommodationService.java
@@ -2,11 +2,45 @@ package uns.ac.rs.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import uns.ac.rs.entity.Accommodation;
 import uns.ac.rs.repository.AccommodationRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class AccommodationService {
 
     @Inject
     AccommodationRepository accommodationRepository;
+
+    public List<Accommodation> getAll() {
+        return accommodationRepository.listAll();
+    }
+
+    public Optional<Accommodation> getById(Long id) {
+        return accommodationRepository.findByIdOptional(id);
+    }
+
+    public void addAccommodation(Accommodation accommodation) {
+        accommodationRepository.persist(accommodation);
+    }
+
+    public void updateAccommodation(Long id, Accommodation updatedAccommodation) {
+        accommodationRepository.findByIdOptional(id).ifPresent(existingAccommodation -> {
+            existingAccommodation.setName(updatedAccommodation.getName());
+            existingAccommodation.setLocation(updatedAccommodation.getLocation());
+            existingAccommodation.setFilters(updatedAccommodation.getFilters());
+            existingAccommodation.setMinGuests(updatedAccommodation.getMinGuests());
+            existingAccommodation.setMaxGuests(updatedAccommodation.getMaxGuests());
+            existingAccommodation.setPriceType(updatedAccommodation.getPriceType());
+            existingAccommodation.setPriceAdjustments(updatedAccommodation.getPriceAdjustments());
+            existingAccommodation.setImages(updatedAccommodation.getImages());
+            accommodationRepository.persist(existingAccommodation);
+        });
+    }
+
+    public void deleteAccommodation(Long id) {
+        accommodationRepository.findByIdOptional(id).ifPresent(accommodationRepository::delete);
+    }
 }
