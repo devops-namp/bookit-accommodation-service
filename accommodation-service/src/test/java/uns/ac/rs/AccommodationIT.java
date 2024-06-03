d2aa396ea6974fdeacda49de6c1d03ff15d97db5
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-03 17:14:31 +0200

    test: created accommodation tests

--- accommodation-service/src/test/java/uns/ac/rs/AccommodationIT.java
+++ accommodation-service/src/test/java/uns/ac/rs/AccommodationIT.java
@@ -4,5 +4,4 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 class AccommodationIT extends AccommodationTest {
-    // Execute the same tests but in packaged mode.
 }
