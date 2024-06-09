cebdee3ac47ba122e6ed90a65f8240655a9fb847
NevenaRadesic <102974378+NevenaRadesic@users.noreply.github.com>
2024-06-09 11:21:52 +0200

    refactor: deleted commented lines

--- accommodation-service/src/main/resources/data-postgres.sql
+++ accommodation-service/src/main/resources/data-postgres.sql
@@ -1,8 +1,8 @@
---INSERT INTO bookit_accommodations (name, location, filters, minguests, maxguests, pricetype)
---VALUES ('Ocean View Apartment', 'Miami Beach, FL', 'wifi,free parking,kitchen', 1, 4, 'price per unit');
+INSERT INTO bookit-accommodations (name, location, filters, minguests, maxguests, pricetype)
+VALUES ('Ocean View Apartment', 'Miami Beach, FL', 'wifi,free parking,kitchen', 1, 4, 'price per unit');
 
---INSERT INTO priceadjustment (fromdate, todate, price, accommodation_id)
---VALUES ('2024-06-01', '2024-06-30', 150.00, 1);
+INSERT INTO priceadjustment (fromdate, todate, price, accommodation_id)
+VALUES ('2024-06-01', '2024-06-30', 150.00, 1);
 
---INSERT INTO images (accommodation_id,images)
---VALUES (1,ARRAY ['https://example.com/image1.jpg', 'https://example.com/image2.jpg']);
+INSERT INTO images (accommodation_id,images)
+VALUES (1,ARRAY ['https://example.com/image1.jpg', 'https://example.com/image2.jpg']);
