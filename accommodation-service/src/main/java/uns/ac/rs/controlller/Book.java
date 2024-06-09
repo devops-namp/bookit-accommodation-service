dc0cf06bcdac5a598226a9924f66e5ac60225aad
NevenaRadesic <102974378+NevenaRadesic@users.noreply.github.com>
2024-06-09 10:15:05 +0200

    build: rabbit-mq added

---
+++ accommodation-service/src/main/java/uns/ac/rs/controlller/Book.java
@@ -0,0 +1,15 @@
+package uns.ac.rs.controlller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    public String title;
+    public String author;
+}
