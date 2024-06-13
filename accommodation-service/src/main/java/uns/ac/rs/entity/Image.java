813cc0db2022fcdc6b73d7c8e7eba78b20a3a1db
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-13 15:55:19 +0200

    fix: added dto for images and changed from string to byte[]

---
+++ accommodation-service/src/main/java/uns/ac/rs/entity/Image.java
@@ -0,0 +1,34 @@
+package uns.ac.rs.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uns.ac.rs.controlller.dto.AccommodationDto;
+import uns.ac.rs.controlller.dto.ImageDto;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name = "image_data", columnDefinition = "BYTEA")
+    private byte[] imageData;
+
+    @ManyToOne
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+
+    public Image(ImageDto imageDto) {
+        this.imageData = imageDto.getImage();
+        this.accommodation = new Accommodation(imageDto.getAccommodationDto());
+    }
+}
