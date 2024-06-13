1643dd16bedf6ff25a59de287ebb754136af3397
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-13 13:16:55 +0200

    feat: accommodations are now creating

--- accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
+++ accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import uns.ac.rs.controlller.dto.AccommodationDto;
+import uns.ac.rs.controlller.dto.PriceAdjustmentDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -34,4 +37,32 @@ public class Accommodation {
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "images", joinColumns = @JoinColumn(name = "accommodation_id"))
     private List<String> images;
+
+    public Accommodation(AccommodationDto accommodationDto) {
+        this.id = accommodationDto.getId();
+        this.name = accommodationDto.getName();
+        this.location = accommodationDto.getLocation();
+        this.filters = accommodationDto.getFilters();
+        this.minGuests = accommodationDto.getMinGuests();
+        this.maxGuests = accommodationDto.getMaxGuests();
+        this.priceType = accommodationDto.getPriceType();
+        this.images = accommodationDto.getImages();
+
+        if (accommodationDto.getPriceAdjustments() != null) {
+            this.priceAdjustments = new ArrayList<>();
+            for (PriceAdjustmentDto priceAdjustmentDto : accommodationDto.getPriceAdjustments()) {
+                PriceAdjustment priceAdjustment = new PriceAdjustment();
+                priceAdjustment.setAccommodation(this);
+                priceAdjustment.setPriceAdjustmentDate(new PriceAdjustmentDate(
+                        priceAdjustmentDto.getPriceAdjustmentDate().getDate(),
+                        priceAdjustmentDto.getPriceAdjustmentDate().getPrice()
+                ));
+                this.priceAdjustments.add(priceAdjustment);
+            }
+        } else {
+            this.priceAdjustments = new ArrayList<>();
+        }
+
+        this.reservations = new ArrayList<>();
+    }
 }
