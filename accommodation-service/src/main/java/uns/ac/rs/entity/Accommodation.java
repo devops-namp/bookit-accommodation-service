004be118bbec3964fa01d5685f189c2bc59cb230
pavleglusac <pavleglusac@gmail.com>
2024-06-13 18:40:26 +0200

    fix: change image type

--- accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
+++ accommodation-service/src/main/java/uns/ac/rs/entity/Accommodation.java
@@ -50,7 +50,7 @@ public class Accommodation {
         this.images = new ArrayList<>();
         for (ImageDto imageData : accommodationDto.getImages()) {
             Image image = new Image();
-            image.setImageData(imageData.getImageAsBytes());
+            image.setImageData(imageData.getBase64Image());
             image.setAccommodation(this);
             this.images.add(image);
         }
