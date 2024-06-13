0b87d9601e18837deeba48898e766a4aec911c40
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-13 16:41:25 +0200

    fix: saving images to database p1

--- accommodation-service/src/main/java/uns/ac/rs/controlller/dto/ImageDto.java
+++ accommodation-service/src/main/java/uns/ac/rs/controlller/dto/ImageDto.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Base64;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImageDto {
-    private AccommodationDto accommodationDto;
-    private byte[] image;
+    private String base64Image;
+
+    public byte[] getImageAsBytes() {
+        return Base64.getDecoder().decode(base64Image);
+    }
 }
